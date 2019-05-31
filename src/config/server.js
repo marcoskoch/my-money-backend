3ad66bcbcddc5b189f338709dc6852386b6e1de9
Marcos Koch <marcos_koch@hotmail.com>
2019-05-31 14:10:57 -0300

    Atualizado conforme curso.

--- src/config/server.js
+++ src/config/server.js
@@ -9,9 +9,10 @@ const queryParser = require('express-query-int')
 server.use(bodyParser.urlencoded({ extended: true }))
 server.use(bodyParser.json())
 server.use(allowCors)
+server.use(queryParser())
 
 server.listen(port, function() {
-  console.log(`Backend is running on port ${port}.`)
+    console.log(`BACKEND is running on port ${port}.`)
 })
 
 module.exports = server
